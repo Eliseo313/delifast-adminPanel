256caf37bc6d1523d5e8d7c496a1dd74cbbccdaa
Eliseo313 <elicarortix58@gmail.com>
2026-04-02 22:14:38 -0600

    f

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 // src/config/config.js
- const URL_BASE = "https://delifastapp.com/api"; //Produccion
+const URL_BASE = "https://delifastapp.com/api"; //Produccion
 //  const URL_BASE = "https://delifastapp.com/apiPruebas";//Pruebas
 // const URL_BASE = "http://localhost:8888/delifast/api/index.php";
 
